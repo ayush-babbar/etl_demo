d3209215a09bd26bffb40774da472360204c946b
ayush-babbar <iamayushbabbar@gmail.com>
2026-02-17 00:56:01 +0530

    Initial commit

---
+++ etl.py
@@ -0,0 +1,18 @@
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    logging.info("ETL Job Started")
+    time.sleep(2)
+    logging.info("Extracting data...")
+    time.sleep(2)
+    logging.info("Transforming data...")
+    time.sleep(2)
+    logging.info("Loading data...")
+    time.sleep(2)
+    logging.info("ETL Job Completed Successfully")
+
+if __name__ == "__main__":
+    main()
